fb195b456c11c7d86d194f46563310f5bc083a90
Talitaventura16 <Talitaventura16@outlook.com>
2023-05-11 17:36:50 -0300

    Ajustei as configurações do forms nas informações de data.

--- utils/forms.php
+++ utils/forms.php
@@ -39,35 +39,29 @@ class criar_ambiente_moodle extends moodleform {
     $this->_form->addElement('text', 'shortname', 'Nome curto do curso', array('disabled' => 'disabled'));
     $this->_form->setDefault('shortname', $shortname);
     $this->_form->setType('shortname', PARAM_TEXT);
-    // $this->_form->addRule('shortname', 'deu pau mane', 'required');
     
     // nome completo do curso
     $fullname = $this->define_campo('fullname');
     $this->_form->addElement('text', 'fullname', 'Nome completo do curso');
     $this->_form->setDefault('fullname', $fullname);
     $this->_form->setType('fullname', PARAM_TEXT);
-    // $this->_form->addRule('fullname', null, 'required');
 
 
     // data de inicio do curso
     $init_date = $this->define_campo('startdate');
-    $init_date_formatted = date('d/m/Y', strtotime($init_date));
-    $this->_form->addElement('text', 'start_date', 'Data de início do curso');
-    $this->_form->setDefault('start_date', $init_date_formatted);
-    $this->_form->setType('start_date', PARAM_TEXT);
-    // $this->_form->addRule('init_date', null, 'required');
+    $init_date_timestamp = strtotime($init_date);
+    $this->_form->addElement('date_selector', 'start_date', 'Data de início do curso');
+    $this->_form->setDefault('start_date', $init_date_timestamp);
 
     // data do fim do curso
     $end_date = $this->define_campo('enddate');
-    $end_date_formatted = date('d/m/Y', strtotime($end_date));
-    $this->_form->addElement('text', 'end_date', 'Data do fim do curso');
-    $this->_form->setDefault('end_date', $end_date_formatted);
-    $this->_form->setType('end_date', PARAM_TEXT);
-    // $this->_form->addRule('end_date', null, 'required');
-
+    $end_date_timestamp = strtotime($end_date);
+    $this->_form->addElement('date_selector', 'end_date', 'Data do fim do curso');
+    $this->_form->setDefault('end_date', $end_date_timestamp);
 
     // Para definir um estilo 
     
+    $end_date_formatted = date('d/m/Y', $end_date_timestamp);
     $end_date_element = $this->_form->getElement('end_date');
     $end_date_element->setLabel('Data do fim do curso <span style="color: #ff0000; font-weight: bold;">' . $end_date_formatted . '</span>');
 
@@ -79,7 +73,6 @@ class criar_ambiente_moodle extends moodleform {
     $this->_form->setDefault('summary', $summary);
     $this->_form->setDefault('summary', $summary);
     $this->_form->setType('summary', PARAM_TEXT);
-    // $this->_form->addRule('summary', null, 'required');
     
     // botao de submit
     $this->_form->addElement('submit', 'criar_ambiente_moodle_submit', 'Criar ambiente');
