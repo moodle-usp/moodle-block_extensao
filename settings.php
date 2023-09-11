4f84344633e9fd2e17e0fe1ce98d7e62490cb567
Potalej <oapotalej@gmail.com>
2023-09-11 08:04:30 -0300

    Issue #7: Padronização do settings.php

--- settings.php
+++ settings.php
@@ -22,75 +22,34 @@ if ($ADMIN->fulltree) {
 
     // Configuracoes de nomes de tabela
     // Por padrao vem com os valores do replicado
-    $setting = new admin_setting_configtext(
-        'block_extensao/tabela_oferecimentoatividadeceu',
-        'OFERECIMENTOATIVIDADECEU',
-        '',
-        'OFERECIMENTOATIVIDADECEU',
-        PARAM_TEXT
-    );
+    $setting = new admin_setting_configtext('block_extensao/tabela_oferecimentoatividadeceu', 'OFERECIMENTOATIVIDADECEU', '', 'OFERECIMENTOATIVIDADECEU', PARAM_TEXT);
     $settings->add($setting);
 
-    $setting = new admin_setting_configtext(
-        'block_extensao/tabela_cursoceu',
-        'CURSOCEU',
-        '',
-        'CURSOCEU',
-        PARAM_TEXT
-    );
+    // Campo para tabela de "CURSOCEU"
+    $setting = new admin_setting_configtext('block_extensao/tabela_cursoceu', 'CURSOCEU', '', 'CURSOCEU', PARAM_TEXT);
     $settings->add($setting);
 
-    $setting = new admin_setting_configtext(
-        'block_extensao/tabela_edicaocursoofeceu',
-        'EDICAOCURSOOFECEU',
-        '',
-        'EDICAOCURSOOFECEU',
-        PARAM_TEXT
-    );
+    // Campo para a tabela de "EDICAOCURSOOFECEU"
+    $setting = new admin_setting_configtext('block_extensao/tabela_edicaocursoofeceu', 'EDICAOCURSOOFECEU', '', 'EDICAOCURSOOFECEU', PARAM_TEXT);
     $settings->add($setting);
 
-    $setting = new admin_setting_configtext(
-        'block_extensao/tabela_ministranteceu',
-        'MINISTRANTECEU',
-        '',
-        'MINISTRANTECEU',
-        PARAM_TEXT
-    );
+    // Campo para a tabela de "MINISTRANTECEU"
+    $setting = new admin_setting_configtext('block_extensao/tabela_ministranteceu', 'MINISTRANTECEU', '', 'MINISTRANTECEU', PARAM_TEXT);
     $settings->add($setting);
     
-    $setting = new admin_setting_configtext(
-        'block_extensao/tabela_emailpessoa',
-        'EMAILPESSOA',
-        '',
-        'EMAILPESSOA',
-        PARAM_TEXT
-    );
+    // Campo para a tabela de "EMAILPESSOA"
+    $setting = new admin_setting_configtext('block_extensao/tabela_emailpessoa', 'EMAILPESSOA', '', 'EMAILPESSOA', PARAM_TEXT);
     $settings->add($setting);
 
-    $setting = new admin_setting_configtext(
-        'block_extensao/tabela_unidade',
-        'UNIDADE',
-        '',
-        'UNIDADE',
-        PARAM_TEXT
-    );
+    // Campo para a tabela de "UNIDADE"
+    $setting = new admin_setting_configtext('block_extensao/tabela_unidade', 'UNIDADE', '', 'UNIDADE', PARAM_TEXT);
     $settings->add($setting);
 
-    $setting = new admin_setting_configtext(
-        'block_extensao/tabela_campus',
-        'CAMPUS',
-        '',
-        'CAMPUS',
-        PARAM_TEXT
-    );
+    // Campo para a tabela de "CAMPUS"
+    $setting = new admin_setting_configtext('block_extensao/tabela_campus', 'CAMPUS', '', 'CAMPUS', PARAM_TEXT);
     $settings->add($setting);
 
-    $setting = new admin_setting_configtext(
-        'block_extensao/tabela_pessoa',
-        'PESSOA',
-        '',
-        'PESSOA',
-        PARAM_TEXT
-    );
+    // Campo para a tabela de "PESSOA"
+    $setting = new admin_setting_configtext('block_extensao/tabela_pessoa', 'PESSOA', '', 'PESSOA', PARAM_TEXT);
     $settings->add($setting);
 }
