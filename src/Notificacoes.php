774ee341c9ae1f550d0a1eff422146a4b0df8065
Potalej <oapotalej@gmail.com>
2024-01-08 10:55:48 -0300

    Correção em settings + Padronização de notificação
    - Correção no settings.php, removendo campo que estava apresentando problemas;
    - Método estático para padronização de notificações em src/Notificacoes.php;
    - Novas strings para tratamento de erros e envio de notificações.

--- src/Notificacoes.php
+++ src/Notificacoes.php
@@ -7,86 +7,91 @@ use core_message\api;
 
 class Notificacoes {
     /**
-     * O objetivo desta classe  eh inserir as notificacoes/mensagens que serao enviadas aos usuarios
-     * @param array $usuario eh o individuo que sera inscrito no Moodle.
-     * @return array|boolean a funcao retorna o envio de uma email
-     *  
+     * O objetivo desta funcao eh enviar um e-mail ao ministrante cuja conta foi criada no moodle
+     * de modo a notifica-lo da criacao da sua conta no moodle.
      */
-    public static function notificacao_inscricao($usuario, $moodle_curso) {
-        /**
-         * O objetivo desta funcao eh enviar um e-mail ao ministrante cuja conta foi criada no moodle
-         * de modo a notifica-lo da criacao da sua conta no moodle.
-         */
-        Global $USER;
-        $USER->firstname; 
+    public static function notificacao_inscricao ($usuario, $moodle_curso) {
+        global $USER;
         try {
-            // verifica se ha um email do professor cadastrado no banco de dados
-            if (empty($usuario->email)) {
-                // mensagem de erro 
-                throw new Exception("Atenção, o professor selecionado não possui um e-mail cadastrado no sistema, a notificação de inscrição não será enviada.");
-            }
-            $userfrom = core_user::get_noreply_user();
-            $userto = $usuario->id;
+            // Verifica se ha um email do professor cadastrado no banco de dados
+            if (empty($usuario->email))
+                throw new Exception(get_string('block_extensao', 'erro_profSemEmail'));
+            
+            // Montagem a mensagem
             date_default_timezone_set('America/Sao_Paulo');
             $data = date('d/m/Y H:i:s', time());
+            
             // Ler a configuração do campo block_extensao/email_body_new_user
             // Substituir os tokens: %profTit, %profAux, %curso e %turma
             // Dica str_replace, %firstname port por $usuario->firstname
-            
             $msg = get_config('block_extensao', 'email_body_new_user');
-
             $msg = str_replace('%profTit', $USER->firstname, $msg); 
             $msg = str_replace('%curso', $moodle_curso->fullname, $msg); 
             $msg = str_replace('%profAux', $usuario->firstname, $msg);
             $msg = str_replace('%turma', $moodle_curso->shortname, $msg);
             $msg = str_replace('%data', $data, $msg);
 
+            // Captura a string do assunto
+            $assunto = get_string('notificacao_inscricao_assunto', 'block_extensao');
 
-            // Verifique se ja existe uma conversa entre os usuarios
-            if (!api::get_conversation_between_users([$userfrom->id, $userto])) {
-                $conversation = api::create_conversation(
+            // Faz o envio da notificacao
+            Notificacoes::enviar_notificacao_sistema($usuario->id, $assunto, $msg, $moodle_curso->id);
+
+        } catch (Exception $e) {
+            \core\notification::error(get_string('erro_padrao', 'block_extensao'));
+            return false;
+        }
+    }
+
+
+    /**
+     * Metodo estatico para padronizar o envio de e-mails pelo e-mail de no-reply.
+     */
+    public static function enviar_notificacao_sistema (int $id_destinatario, string $assunto, string $msg, int $curso_id) {
+        try {
+            // O remetente eh o usuario no-reply padrao.
+            $remetente = core_user::get_noreply_user();
+            
+            // Verifica se ja existe uma conversa entre os usuarios
+            if (!api::get_conversation_between_users([$remetente->id, $id_destinatario])) {
+                $conversa = api::create_conversation(
                     api::MESSAGE_CONVERSATION_TYPE_INDIVIDUAL,
                     [
-                        $userfrom->id,
-                        $userto
+                        $remetente->id,
+                        $id_destinatario
                     ]
                 );
             }
-
-            // Crie o objeto de mensagem
+    
+            // Cria objeto de mensagem
             $mensagem = new message();
-            $mensagem->component = 'moodle';
-            $mensagem->name = 'instantmessage';
-            $mensagem->userfrom = $userfrom->id;
-            $mensagem-> userto = $userto;
-            $mensagem->subject = 'Nova mensagem';
-            $mensagem->fullmessage = $msg;
+            $mensagem->component         = 'moodle';
+            $mensagem->name              = 'instantmessage';
+            $mensagem->userfrom          = $remetente->id;
+            $mensagem->userto            = $id_destinatario;
+            $mensagem->subject           = $assunto;
             $mensagem->fullmessageformat = FORMAT_MARKDOWN;
-            $mensagem->fullmessagehtml = $msg;
-            $mensagem->smallmessage = $msg;
-            $mensagem->notification = 0;
-            $mensagem->contexturl = '';
-            $mensagem->contexturlname = 'Nome do Contexto';
-
-            // Obtem o email de configuracao padrao do Moodle
+            $mensagem->fullmessage       = $msg;
+            $mensagem->fullmessagehtml   = $msg;
+            $mensagem->smallmessage      = $msg;
+            $mensagem->notification      = 0;
+            $mensagem->contexturl        = '';
+            $mensagem->contexturlname    = '';
+            $mensagem->courseid          = $curso_id;
+    
+            // Obtem o e-mail de configuracao padrao do Moodle
             $mensagem->replyto = get_config('moodle', 'replyto');
-            $content = array('*' => array('header' => '', 'footer' => ''));
+            $content = array("*" => array('header'=>'', 'footer'=>''));
             $mensagem->set_additional_content('email', $content);
-            $mensagem->courseid = 107;
+            
+            // Envia a mensagem
+            $message_id = message_send($mensagem);
+            return $message_id;
 
-            // Envie a mensagem
-            $messageid = message_send($mensagem);
-
-            if ($messageid) {
-                // Mensagem enviada com sucesso
-                return true;
-            } else {
-                // Erro ao enviar a mensagem
-                return false;
-            }
         } catch (Exception $e) {
             // Lida com excecoes
-            return false;
+            \core\notification::error(get_string('erro_padrao', 'block_extensao'));
+            return false; 
         }
     }
 }
