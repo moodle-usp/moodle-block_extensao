dec4ca354f443d663e3c77288855b7647fb5bd27
Thiago Gomes Verissimo <verissimotgv@gmail.com>
2024-04-12 17:32:10 -0300

    vendor included

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'moodle/extensao',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => 'dcaa1fa302d5a6afa8e590f573f6141e15aa547f',
+        'type' => 'moodle-plugin',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'moodle/extensao' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => 'dcaa1fa302d5a6afa8e590f573f6141e15aa547f',
+            'type' => 'moodle-plugin',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
